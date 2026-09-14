ce9d4dcf3791c9ba1e7d44fd7ac90ab1a7a1ee1e
Light <L@>
2026-09-14 21:58:08 +0100

    Fixed eveyrthing,  the #1,2,3 color cordination could have been more efficient but it all works perfectly fine!

--- Mario Kart VR Leaderboard/Includes.h
+++ Mario Kart VR Leaderboard/Includes.h
@@ -1,15 +1,38 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <algorithm>
+#include <cstdlib>
+#include <conio.h>
+#include <Windows.h>
 
+using namespace std;
 
+void clear()
+{
+	system("CLS");
+}
 
+void pause()
+{
+	this_thread::sleep_for(chrono::seconds(2));
+	clear();
+}
 
+void space()
+{
+	cout << endl;
+}
 
-
-
-
-
-
+void getKey()
+{
+	space();
+	cout << "Press any key to return." << endl;
+	_getch();
+}
 
 void initConsole()
 {
@@ -21,3 +44,31 @@ void initConsole()
 	GetConsoleMode(hConsole, &mode);
 	SetConsoleMode(hConsole, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
 }
+
+
+// -- Text colours --
+const char* black = "\033[30m";
+const char* red = "\033[31m";
+const char* green = "\033[32m";
+const char* yellow = "\033[33m";
+const char* blue = "\033[34m";
+const char* magenta = "\033[35m";
+const char* cyan = "\033[36m";
+const char* white = "\033[37m";
+const char* gold = "\033[93m"; 
+const char* silver = "\033[38;5;245m";
+const char* bronze = "\033[33m";
+
+// -- Bright versions --
+const char* brightRed = "\033[91m";
+const char* brightGreen = "\033[92m";
+const char* brightYellow = "\033[93m";
+const char* brightBlue = "\033[94m";
+const char* brightMagenta = "\033[95m";
+const char* brightCyan = "\033[96m";
+const char* brightWhite = "\033[97m";
+
+// -- Styles --
+const char* bold = "\033[1m";
+const char* underline = "\033[4m";
+const char* reset = "\033[0m";
--- Mario Kart VR Leaderboard/Mario Kart VR Leaderboard.cpp
+++ Mario Kart VR Leaderboard/Mario Kart VR Leaderboard.cpp
@@ -1,11 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <thread>
-#include <chrono>
-#include <algorithm>
-#include <cstdlib>
-#include <conio.h>
-#include <Windows.h>
+#include "Includes.h"
 
 using namespace std;
 
@@ -30,7 +23,7 @@ bool byPos(const Player& a, const Player& b)
 auto main() -> int
 {
 	SetConsoleTitleA("Mario Kart Versus Rating");
-	srand(time(0));
+	initConsole();
 	Player player{};
 
 	vector<string>availableNames = 
@@ -48,25 +41,43 @@ auto main() -> int
 		"Birdo",
 		"Spike",
 	};
+
+	vector<int>availableNums =
+	{
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12
+	};
 	
+	vector<int>takenNums;
 	vector<string>takenNames;
 	vector<Player>Entities;
-
-	while (true)
-	{
-
-		//int random = rand() % takenNames.size();
-		int score = rand() % 10000 + 1000;
-		int place = rand() % 12 + 1;
 
 		while (!availableNames.empty())
 		{
 			int index = rand() % availableNames.size();
+			int place = rand() % availableNums.size();
+			int score = rand() % 9700 + 1000;
+
 			takenNames.push_back(availableNames[index]);
 			availableNames.erase(availableNames.begin() + index);
+
+			takenNums.push_back(availableNums[place]);
+			availableNums.erase(availableNums.begin() + place);
+
 			player.name = takenNames.back();
 			player.VR = score;
-			player.position = place;
+			player.position = takenNums.back();
+
 			Entities.push_back(player);
 		}
 
@@ -74,7 +85,14 @@ auto main() -> int
 
 		for (size_t i = 0; i < Entities.size(); i++)
 		{
-			cout << "#" << player.position << " " << player.name << " " << player.VR << endl;
+			if (Entities[i].position == 1) cout << gold << "#" << Entities[i].position << reset << " " << Entities[i].name << " " << Entities[i].VR << endl;
+			else if (Entities[i].position == 2) cout << silver << "#" << Entities[i].position << reset << " " << Entities[i].name << " " << Entities[i].VR << endl;
+			else if (Entities[i].position == 3) cout << bronze << "#" << Entities[i].position << reset << " " << Entities[i].name << " " << Entities[i].VR << endl;
+
+			if (Entities[i].position == 1 || Entities[i].position == 2 || Entities[i].position == 3) continue;
+
+			cout << "#" << Entities[i].position << " " << Entities[i].name << " " << Entities[i].VR << endl;
 		}
-	}
+		getKey();
+		return 0;
 }
